0e1e79a7fada7b6249d2652f390426e9d2cb0890
Tereami <undress.my.future@gmail.com>
2021-05-23 22:19:37 +0300

    add main controller

---
+++ TestShopAspnet/Controllers/MainController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestShopAspnet.Controllers
+{
+    public class MainController : Controller
+    {
+        private IConfiguration Configuration;
+
+        public MainController(IConfiguration conf)
+        {
+            Configuration = conf;
+        }
+
+
+        public IActionResult Index()
+        {
+            return Content("Hello from main controller index action");
+        }
+
+        public IActionResult Personal()
+        {
+            return Content(Configuration["MyMessage"]);
+        }
+    }
+}
