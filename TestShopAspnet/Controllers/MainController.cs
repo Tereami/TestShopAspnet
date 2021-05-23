6e9ca9d2b60322675f3176ac1642f9ce69dfd2e5
Tereami <undress.my.future@gmail.com>
2021-05-23 22:42:00 +0300

    add view - list persons

--- TestShopAspnet/Controllers/MainController.cs
+++ TestShopAspnet/Controllers/MainController.cs
@@ -4,11 +4,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestShopAspnet.Models;
 
 namespace TestShopAspnet.Controllers
 {
     public class MainController : Controller
     {
+        private static readonly List<Person> _persons = new List<Person>
+        {
+            new Person("Иван", "Иванов", 21),
+            new Person("Пётр", "Петров", 31),
+            new Person("Сидор", "Сидоров", 41)
+        };
+
         private IConfiguration Configuration;
 
         public MainController(IConfiguration conf)
@@ -24,7 +32,7 @@ namespace TestShopAspnet.Controllers
 
         public IActionResult Personal()
         {
-            return Content(Configuration["MyMessage"]);
+            return View(_persons);
         }
     }
 }
