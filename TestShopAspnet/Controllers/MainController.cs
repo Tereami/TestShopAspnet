a0408d9165bdc215f3d05896ac1c150ea95b178e
Tereami <undress.my.future@gmail.com>
2021-05-27 11:42:02 +0300

    add all views and links in main menu

--- TestShopAspnet/Controllers/MainController.cs
+++ TestShopAspnet/Controllers/MainController.cs
@@ -44,5 +44,45 @@ namespace TestShopAspnet.Controllers
             }
             return View(checkPers);
         }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult BlogSingle()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
     }
 }
