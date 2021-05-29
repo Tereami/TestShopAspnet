07aaf62f4dda464e873052d9d9d86f5a51bb89ec
Tereami <undress.my.future@gmail.com>
2021-05-29 22:58:33 +0300

    add Edit form and action

--- TestShopAspnet/Controllers/PersonsController.cs
+++ TestShopAspnet/Controllers/PersonsController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using TestShopAspnet.Models;
 using TestShopAspnet.Services.Interfaces;
+using TestShopAspnet.ViewModels;
 
 namespace TestShopAspnet.Controllers
 {
@@ -41,11 +42,26 @@ namespace TestShopAspnet.Controllers
         public IActionResult Edit(int id)
         {
             Person checkPers = _personsService.Get(id);
-            if (checkPers == null)
-            {
+            if (checkPers == null)            
                 return new NotFoundResult();
-            }
-            return View(checkPers);
+            PersonViewModel persvm = new PersonViewModel()
+            {
+                Id = checkPers.Id,
+                Name = checkPers.Name,
+                Surname = checkPers.Surname,
+                Age = checkPers.Age,
+                Position = checkPers.Position
+            };
+            return View(persvm);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(PersonViewModel vm)
+        {
+            Person p = new Person(vm.Name, vm.Surname, vm.Age, vm.Position);
+            p.Id = vm.Id;
+            _personsService.Update(p);
+            return RedirectToAction("Index");
         }
 
         public IActionResult Delete(int id)
--- TestShopAspnet/ViewModels/PersonViewModel.cs
+++ TestShopAspnet/ViewModels/PersonViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestShopAspnet.Models;
 
 namespace TestShopAspnet.ViewModels
 {
@@ -12,5 +13,6 @@ namespace TestShopAspnet.ViewModels
         public string Surname { get; set; }
         public int Age { get; set; }
         public string Position { get; set; }
+
     }
 }
