2684ba979e24c99cff400d2f9807ff42833580a5
Tereami <undress.my.future@gmail.com>
2021-06-06 13:45:20 +0300

    add brands and sections testdata

---
+++ TestShopAspnet/Services/InMemoryProductData.cs
@@ -0,0 +1,22 @@
+﻿using DomainModel.Enitities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestShopAspnet.Services.Interfaces;
+
+namespace TestShopAspnet.Services
+{
+    public class InMemoryProductData : IProductData
+    {
+        public IEnumerable<Brand> GetBrands()
+        {
+            
+        }
+
+        public IEnumerable<Section> GetSections()
+        {
+            
+        }
+    }
+}
---
+++ TestShopAspnet/Services/Interfaces/IProductData.cs
@@ -0,0 +1,14 @@
+﻿using DomainModel.Enitities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestShopAspnet.Services.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Brand> GetBrands();
+        IEnumerable<Section> GetSections();
+    }
+}
