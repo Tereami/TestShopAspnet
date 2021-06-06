5eb33cb967cc6e700d75a31444975431856ca428
Tereami <undress.my.future@gmail.com>
2021-06-06 12:17:13 +0300

    add business-model and base classes

---
+++ DomainModel/Enitities/Base/NamedEntity.cs
@@ -0,0 +1,14 @@
+﻿using DomainModel.Enitities.Base.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.Enitities.Base
+{
+    public abstract class NamedEntity : Entity, INamedEntity
+    {
+        public string Name { get; set; }
+    }
+}
