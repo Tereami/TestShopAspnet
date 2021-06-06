a1b41248b365a28b5a8f0fc23e9ba833dfc3f18e
Tereami <undress.my.future@gmail.com>
2021-06-06 17:32:19 +0300

    add sections view components

---
+++ TestShopAspnet/ViewModels/SectionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestShopAspnet.ViewModels
+{
+    public class SectionViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Order { get; set; }
+
+        public List<SectionViewModel> ChildSections { get; set; } = new();
+    }
+}
