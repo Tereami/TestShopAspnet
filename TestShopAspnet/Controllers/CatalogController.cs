dda220fa31c1e23a7db047bd01fafe62c047c8cf
Tereami <undress.my.future@gmail.com>
2021-06-06 18:12:09 +0300

    add products controller and view

---
+++ TestShopAspnet/Controllers/CatalogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestShopAspnet.Services.Interfaces;
+
+namespace TestShopAspnet.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IProductData _service;
+
+        public CatalogController(IProductData service)
+        {
+            _service = service;
+        }
+
+
+        public IActionResult Index(int? BrandId, int? SectionId)
+        {
+            return View();
+        }
+    }
+}
