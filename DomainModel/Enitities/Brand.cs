a52454882e78038038763c992ac61ce1ac666272
Tereami <undress.my.future@gmail.com>
2021-06-06 13:29:54 +0300

    add classes Brand and Section

---
+++ DomainModel/Enitities/Brand.cs
@@ -0,0 +1,15 @@
+﻿using DomainModel.Enitities.Base;
+using DomainModel.Enitities.Base.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.Enitities
+{
+    public class Brand : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+    }
+}
