1f8fc5aeb46f46110f98b298ce3cbaa0c779a883
Tereami <undress.my.future@gmail.com>
2021-06-12 18:07:37 +0300

    create the db context

---
+++ DataAccessLayer/Context/DB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DomainModel.Enitities;
+
+namespace DataAccessLayer.Context
+{
+    public class DB : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Section> Sections { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DB(DbContextOptions<DB> options) : base(options)
+        {
+
+        }
+    }
+}
