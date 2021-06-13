758fb4385082575434028cae80b6074ef213b43d
Tereami <undress.my.future@gmail.com>
2021-06-13 19:18:25 +0300

    add registration

--- TestShopAspnet/Controllers/Account.cs
+++ TestShopAspnet/Controllers/Account.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using DomainModel.Identity;
+using TestShopAspnet.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +11,43 @@ namespace TestShopAspnet.Controllers
 {
     public class Account : Controller
     {
-        public IActionResult Register() => View();
+        private readonly UserManager<User> _UserManager;
+        private readonly SignInManager<User> _SignInManager;
+
+        public Account(UserManager<User> UserManager, SignInManager<User> SignInManager)
+        {
+            _UserManager = UserManager;
+            _SignInManager = SignInManager;
+        }
+
+        public IActionResult Register() => View(new RegisterUserViewModel());
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            User user = new User
+            {
+                UserName = model.Username,
+            };
+
+            IdentityResult register_result = await _UserManager.CreateAsync(user, model.Password);
+            if(register_result.Succeeded)
+            {
+                await _SignInManager.SignInAsync(user, false); //временный вход без пароля
+
+                RedirectToAction("Index", "Main");
+            }
+
+            foreach(IdentityError error in register_result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+            return View(model);
+        }
+       
 
         public IActionResult Login() => View();
 
---
+++ TestShopAspnet/ViewModels/RegisterUserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestShopAspnet.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set;}
+
+        [Required]
+        [Display(Name = "Повторите пароль")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))] //должно совпадать с паролем
+        public string PasswordConfirm { get; set; }
+    }
+}
