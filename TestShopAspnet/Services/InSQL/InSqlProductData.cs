fecb7b5e8a69531a3654ac3aaec626ac49dffc7b
Tereami <undress.my.future@gmail.com>
2021-06-14 23:18:05 +0300

    Get products by ids array

--- TestShopAspnet/Services/InSQL/InSqlProductData.cs
+++ TestShopAspnet/Services/InSQL/InSqlProductData.cs
@@ -37,14 +37,21 @@ namespace TestShopAspnet.Services.InSQL
 
             if (filter != null)
             {
-                if (filter.BrandId != null)
-                    products = products.Where(i => i.BrandId == filter.BrandId);
-                if (filter.SectionId != null)
-                    products = products.Where(i => i.SectionId == filter.SectionId);
-                if (filter.Limit != null)
+                if (filter.Ids != null && filter.Ids.Length > 0)
                 {
-                    int limit2 = (int)filter.Limit;
-                    products = products.Take(limit2);
+                    products.Where(p => filter.Ids.Contains(p.Id));
+                }
+                else
+                {
+                    if (filter.BrandId != null)
+                        products = products.Where(i => i.BrandId == filter.BrandId);
+                    if (filter.SectionId != null)
+                        products = products.Where(i => i.SectionId == filter.SectionId);
+                    if (filter.Limit != null)
+                    {
+                        int limit2 = (int)filter.Limit;
+                        products = products.Take(limit2);
+                    }
                 }
             }
 
