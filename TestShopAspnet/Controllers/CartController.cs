76563300145568b21bf56d39a44c3d7c2b70d5e7
Tereami <undress.my.future@gmail.com>
2021-06-14 23:49:51 +0300

    prepare the cart view

---
+++ TestShopAspnet/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestShopAspnet.Services.Interfaces;
+
+namespace TestShopAspnet.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService CartService)
+        {
+            _cartService = CartService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_cartService.GetViewModel());
+        }
+    }
+}
