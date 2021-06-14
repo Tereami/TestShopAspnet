5bb2e67854b0fd7253140ca75bfda7aa97ea31e2
Tereami <undress.my.future@gmail.com>
2021-06-14 21:40:39 +0300

    prepare for the cart

---
+++ TestShopAspnet/Services/InCookies/InCookiesCartService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestShopAspnet.Services.Interfaces;
+using TestShopAspnet.ViewModels;
+
+namespace TestShopAspnet.Services.InCookies
+{
+    public class InCookiesCartService : ICartService
+    {
+        private readonly IHttpContextAccessor _httpAccessor;
+        private readonly IProductData _productData;
+        private readonly string _cartName;
+
+        public InCookiesCartService(
+            IHttpContextAccessor HttpAccessor, 
+            IProductData ProductData
+            )
+        {
+            _httpAccessor = HttpAccessor;
+            _productData = ProductData;
+
+            var user = HttpAccessor.HttpContext.User;
+            string username = "-Anonymous";
+            if (user.Identity.IsAuthenticated)
+                username = "-" + user.Identity.Name;
+            _cartName = "TestShopAspnet.Cart" + username;
+        }
+
+        public void Add(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Decrement(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CartViewModel GetViewModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
